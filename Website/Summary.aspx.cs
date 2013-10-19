e563bc19bc77b1110004c02eb242c09c1e87024c
D6C92FE5 <D6C92FE5@hotmail.com>
2013-10-19 20:14:19 +0800

    添加 X-UA-Compatible

--- Website/Summary.aspx.cs
+++ Website/Summary.aspx.cs
@@ -9,6 +9,6 @@ public partial class Summary : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        Response.AppendHeader("X-UA-Compatible", "IE=edge,chrome=1");
     }
 }
